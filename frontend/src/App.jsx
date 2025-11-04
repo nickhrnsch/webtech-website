555d928f6d0e2d482e322e8cda9c9bcdabc4db26
nickhrnsch <nicklasharnisch05@gmail.com>
2025-11-04 09:55:58 +0100

    back to normal (h1 added)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomepageLayout from './HomepageLayout.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <HomepageLayout/>
+      <h1>Test</h1>
     </>
   )
 }
