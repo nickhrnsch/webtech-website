7f1aba129e423dc895b85203e6905ca37a8821f8
nickhrnsch <nicklasharnisch05@gmail.com>
2025-11-04 21:15:24 +0100

    Beer!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import beer from "./assets/beer.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>I love beer</h1>
+      <img src={beer} width={1000} alt="Beer" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
