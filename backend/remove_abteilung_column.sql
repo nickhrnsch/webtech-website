8eca4288215d5d4043c77095526ceec3757cf913
nickhrnsch <nicklasharnisch05@gmail.com>
2026-02-25 20:14:44 +0100

    Ein bisschen aufgeräumt.

--- backend/remove_abteilung_column.sql
+++
@@ -1,6 +0,0 @@
--- Entfernt die Spalte "abteilung" aus der users-Tabelle.
--- Für moderne SQLite-Versionen (>= 3.35) oder andere SQL-Datenbanken,
--- die DROP COLUMN unterstützen:
-
-ALTER TABLE users DROP COLUMN abteilung;
-
